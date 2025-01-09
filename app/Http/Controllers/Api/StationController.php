aec8aeb1689511ffedc561c2bf8cb25b1c7ea0fc
issamchlf <issamchellaf734@gmail.com>
2025-01-09 14:00:58 +0100

    feat:add crud of station

--- app/Http/Controllers/Api/StationController.php
+++ app/Http/Controllers/Api/StationController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Station;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class StationController extends Controller
 {
@@ -12,23 +13,31 @@ class StationController extends Controller
      */
     public function index()
     {
-        //
+        $stations = Station::all();
+        return response()->json($stations);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    //public function create()
+    //{
         //
-    }
+    //}
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'city' => 'required',
+            'total_collected_fee' => 'required',
+        ]);
+
+        $station = Station::create($validatedData); 
+        return response()->json($station, 201);
     }
 
     /**
@@ -36,23 +45,26 @@ class StationController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $station = Station::findOrFail($id);
+        return response()->json($station, 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $station = Station::findOrFail($id);
+        if (!$station) {
+            return response()->json(['message' => 'Station not found'], 404);
+        }
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'city' => 'required',
+            'total_collected_fee' => 'required',
+        ]);
+
+        $station = Station::findOrFail($id);
+        $station->update($validatedData);
+        return response()->json($station, 200); 
     }
 
     /**
@@ -60,6 +72,11 @@ class StationController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $station = Station::findOrFail($id);
+        if (!$station) {
+            return response()->json(['message' => 'Station not found'], 404);
+        }
+        $station->delete();
+        return response()->json(null, 204); 
     }
 }
