f3f8667b8d900732cf35ecd1bea1425e4f43ba16
issamchlf <issamchellaf734@gmail.com>
2025-01-21 08:30:58 +0100

    feat:add crud routes & views routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\StationController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/station', [StationController::class, 'index'])->name('station.index');
+Route::post('/station', [StationController::class, 'store'])->name('station.store');
+Route::get('/station/{id}', [StationController::class, 'show'])->name('station.show');
+Route::put('/station/{id}', [StationController::class, 'update'])->name('station.update');
+Route::delete('/station/{id}', [StationController::class, 'destroy'])->name('station.destroy');
+
+Route::get('/station/{id}/vehicles', [StationController::class, 'vehicles'])->name('station.vehicles');
+Route::post('/station/{id}/vehicles', [StationController::class, 'registerVehicle'])->name('station.registerVehicle');
+Route::post('/station/{id}/toll', [StationController::class, 'registerToll'])->name('station.registerToll');
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
-use App\Http\Controllers\StationController;
 use App\Models\Station;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StationController;
+use App\Http\Controllers\VehicleController;
 
 Route::get('/', function () {
     return view('home');
 });
 Route::get('station.index', [StationController::class, 'index'])->name('station.index');
+Route::get('station.show', [StationController::class, 'show'])->name('station.show');
+
+Route::get('vehicle.index', [VehicleController::class, 'index'])->name('vehicle.index');
+Route::get('vehicle.show', [VehicleController::class, 'show'])->name('vehicle.show');
+
 
